25b87675b3ff779b4fc3592611a9400b54b1bdae
KristineBerzina <k.beerzina@gmail.com>
2021-06-30 13:58:02 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nrOne = "9";
+        String nrTwo = "6";
+
+        int nr1 = Integer.parseInt(nrOne);
+        int nr2 = Integer.parseInt(nrTwo);
+
+        System.out.println(nrOne + " + " + nrTwo + " = " + (nr1 + nr2));
+
+    }
+}
